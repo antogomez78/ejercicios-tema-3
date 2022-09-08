56321b99cbb531202f9197c5398860dbc6fbd57a
antogomez78 <antogomez57@gmail.com>
2022-09-08 20:11:20 -0300

    tarea corregida

--- Main.java
+++ Main.java
@@ -1,28 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-       coche MiCoche = new coche();
-       MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
-        MiCoche.AgregarPuerta();
+        suma(20, 15, 8);
 
+        coche MiCoche = new coche();
+        MiCoche.AgregarPuerta();
+        MiCoche.AgregarPuerta();
         System.out.println(MiCoche.puertas);
     }
 
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
 
-
-
-
-
+    }
 }
 
-class coche{
-    public int puertas = 0;
-    public void AgregarPuerta(){
-     this.puertas++;
+class coche {
+    public int puertas = 4;
 
-        }
+    public void AgregarPuerta() {
+        this.puertas++;
+
+    }
 }
